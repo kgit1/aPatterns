323900738200f21f5a6213cb800ba68322eaafba
kgit1 <kgit1@rambler.ru>
2017-01-23 00:00:46 +0200

    up

--- src/code/Money.java
+++ src/code/Money.java
@@ -5,15 +5,9 @@ public class Money {
 	public static void main(String[] args) {
 		int i = calculateYears(1000, 0.05, 0.18, 1100);
 		System.out.println(i);
-//		i = calculateYears(1000, 0.01625, 0.18, 1200);
-//		System.out.println(i);
-//		i = calculateYears(1000, 0.05, 0.18, 1000);
-//		System.out.println(i);
-//		System.out.println(1000 * 0.05 - 1000 * 0.05 * 0.18);
-//		System.out.println(1000 * 0.01625);
-//		System.out.println( 1000 * 0.01625 * 0.18);
-//		System.out.println(1000 * 0.01625- 1000 * 0.01625 * 0.18);
-//		System.out.println(200/13.325);
+		i = calculateYears(1000, 0.01625, 0.18, 1200);
+		System.out.println(i);
+		i = calculateYears(1000, 0.05, 0.18, 1000);
 	}
 
 	public static int calculateYears(double principal, double interest,
@@ -21,8 +15,7 @@ public class Money {
 		int y = 0;
 		double sum = principal;
 		while (sum < desired) {
-			sum += principal * interest - principal * interest * tax;
-			System.out.println("sum " + sum);
+			sum += sum * interest - sum * interest * tax;
 			y++;
 		}
 		return y;
