336c42490edd127dbd895d1dc72af7d9e909450c
kgit1 <kgit1@rambler.ru>
2017-01-23 00:05:44 +0200

    up

--- src/code/Money.java
+++ src/code/Money.java
@@ -3,11 +3,12 @@ package code;
 public class Money {
 
 	public static void main(String[] args) {
-		int i = calculateYears(1000, 0.05, 0.18, 1100);
+		int i = calculateYearsRefactored(1000, 0.05, 0.18, 1100);
 		System.out.println(i);
-		i = calculateYears(1000, 0.01625, 0.18, 1200);
+		i = calculateYearsRefactored(1000, 0.01625, 0.18, 1200);
 		System.out.println(i);
-		i = calculateYears(1000, 0.05, 0.18, 1000);
+		i = calculateYearsRefactored(1000, 0.05, 0.18, 1000);
+		System.out.println(i);
 	}
 
 	public static int calculateYears(double principal, double interest,
@@ -19,5 +20,15 @@ public class Money {
 			y++;
 		}
 		return y;
+	}
+
+	public static int calculateYearsRefactored(double principal,
+			double interest, double tax, double desired) {
+		int years = 0;
+		while (principal < desired) {
+			principal += principal * interest * (1 - tax);
+			years++;
+		}
+		return years;
 	}
 }
