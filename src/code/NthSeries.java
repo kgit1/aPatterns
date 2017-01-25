1d866a01bf7055a66536e40630beea2176e247d1
kgit1 <kgit1@rambler.ru>
2017-01-25 17:38:03 +0200

    up

---
+++ src/code/NthSeries.java
@@ -0,0 +1,54 @@
+package code;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+public class NthSeries {
+
+	public static void main(String[] args) {
+		System.out.println(seriesSum(1));
+		System.out.println("------------");
+		System.out.println(seriesSum(2));
+		System.out.println("------------");
+		System.out.println(seriesSum1(5));
+		System.out.println("------------");
+		System.out.println(seriesSum1(6));
+		System.out.println("------------");
+		System.out.println(seriesSum1(7));
+		System.out.println("------------");
+		System.out.println(seriesSum2(8));
+		System.out.println("------------");
+		System.out.println(seriesSum2(9));
+		System.out.println("------------");
+		System.out.println(seriesSum2(10));
+		System.out.println("------------");
+		System.out.println(seriesSum2(58));
+
+	}
+
+	public static String seriesSum(int n) {
+		double sum = 0;
+		for (int i = 1; i < n * 3; i += 3) {
+			sum += 1.0 / i;
+		}
+		return String.format("%.2f", sum);
+	}
+
+	public static String seriesSum1(int n) {
+		String s = "";
+		float f = 0;
+		for (int i = 0; i < n; i++) {
+			f += 1.0 / (1 + 3 * i);
+		}
+		return s += BigDecimal.valueOf(f).setScale(2, BigDecimal.ROUND_HALF_UP);
+	}
+
+	public static String seriesSum2(int n) {
+		DecimalFormat df = new DecimalFormat("0.00");
+		double result = 0;
+		for (int i = 1; i <= n; i++) {
+			result = result + 1.0 / (3 * i - 2);
+		}
+		return df.format(result);
+	}
+}
