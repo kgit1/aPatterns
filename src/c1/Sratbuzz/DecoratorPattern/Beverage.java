64b3a73940e9abbb20a76dbedf8d2bf6e65eff06
kgit1 <kgit1@rambler.ru>
2017-01-31 00:05:33 +0200

    up

--- src/c1/Sratbuzz/DecoratorPattern/Beverage.java
+++ src/c1/Sratbuzz/DecoratorPattern/Beverage.java
@@ -1,7 +1,11 @@
 package c1.Sratbuzz.DecoratorPattern;
 
+import java.awt.Color;
+
 public abstract class Beverage {
 	String description = "Unknown Beverage";
+	
+	Color color = new Color(123);
 
 	public String getDescription() {
 		return description;
@@ -10,9 +14,9 @@ public abstract class Beverage {
 	public abstract double cost();
 
 	// to do
-	public void getSize() {
-	};
-
-	public void setSize() {
-	};
+//	public void getSize() {
+//	};
+//
+//	public void setSize() {
+//	};
 }
