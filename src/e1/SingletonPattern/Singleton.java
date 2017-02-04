a09e28584337f908b6bfc6bb0b4eeb1a507a7374
kgit1 <kgit1@rambler.ru>
2017-02-04 21:29:08 +0200

    singleton pattern

---
+++ src/e1/SingletonPattern/Singleton.java
@@ -0,0 +1,16 @@
+package e1.SingletonPattern;
+
+public class Singleton {
+	private static Singleton uniqueInstance;
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (uniqueInstance == null) {
+			return new Singleton();
+		}
+		return uniqueInstance;
+	}
+
+}
