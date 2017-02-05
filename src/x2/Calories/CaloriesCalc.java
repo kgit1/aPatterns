81d99164f448a568ba5d0897e8d4b7d48b061a24
kgit1 <kgit1@rambler.ru>
2017-02-05 16:33:43 +0200

    up

--- src/x2/Calories/CaloriesCalc.java
+++ src/x2/Calories/CaloriesCalc.java
@@ -17,7 +17,7 @@ public class CaloriesCalc {
 		System.out.println("Katch-McArdle formula :              " + calcKatchMcArdle(99, 0.3, 1));
 		System.out.println("World Health Organization formula :  " + calcWorldHealthOrganization(99, 1));
 		System.out.println("Tkachenko formula :                  " + calcTkachenko(99, 1.835, 1));
-		System.out.println("-------------------------------------");
+		System.out.println("--------------------------------------------");
 
 		System.out.println("Body Mass Index");
 		System.out.println(
@@ -28,30 +28,8 @@ public class CaloriesCalc {
 
 	// Harris–Benedict equation (formula)
 	public static double calcHarrisBenedict(double bodyWeight, double bodyHeight, int age, double activity) {
-		DecimalFormat df = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US));
-		// Little to no exercise Daily kilocalories needed = BMR x 1.2
-		// Light exercise (1–3 days per week) Daily kilocalories needed = BMR x
-		// 1.375
-		// Moderate exercise (3–5 days per week) Daily kilocalories needed = BMR
-		// x 1.55
-		// Heavy exercise (6–7 days per week) Daily kilocalories needed = BMR x
-		// 1.725
-		// Very heavy exercise (twice per day, extra heavy workouts) Daily
-		// kilocalories needed = BMR x 1.9
-		if (activity == 1) {
-			activity = 1.2;
-		} else if (activity == 2) {
-			activity = 1.375;
-		} else if (activity == 3) {
-			activity = 1.55;
-		} else if (activity == 4) {
-			activity = 1.725;
-		} else if (activity == 5) {
-			activity = 1.9;
-		}
-		double bmr = activity * (88.362 + (13.397 * bodyWeight) + (4.799 * bodyHeight) - (5.677 * age));
-		System.out.println(df.format(bmr));
-		return Double.parseDouble(df.format(bmr));
+		double bmr = activity(activity) * (88.362 + (13.397 * bodyWeight) + (4.799 * bodyHeight) - (5.677 * age));
+		return doubleToTwoDigits(bmr);
 	}
 
 	// Mifflin - St Jeor equation (formula)
@@ -60,118 +38,33 @@ public class CaloriesCalc {
 	// equations2. The Mifflin-St Jeor was
 	// found to be the most accurate.
 	public static double calcMifflinStJeor(double bodyWeight, double bodyHeight, int age, double activity) {
-		DecimalFormat df = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US));
-		// Little to no exercise Daily kilocalories needed = BMR x 1.2
-		// Light exercise (1–3 days per week) Daily kilocalories needed = BMR x
-		// 1.375
-		// Moderate exercise (3–5 days per week) Daily kilocalories needed = BMR
-		// x 1.55
-		// Heavy exercise (6–7 days per week) Daily kilocalories needed = BMR x
-		// 1.725
-		// Very heavy exercise (twice per day, extra heavy workouts) Daily
-		// kilocalories needed = BMR x 1.9
-		if (activity == 1) {
-			activity = 1.2;
-		} else if (activity == 2) {
-			activity = 1.375;
-		} else if (activity == 3) {
-			activity = 1.55;
-		} else if (activity == 4) {
-			activity = 1.725;
-		} else if (activity == 5) {
-			activity = 1.9;
-		}
-		double bmr = activity * (9.99 * bodyWeight + 6.25 * bodyHeight - 4.92 * age + 5);
-		return Double.parseDouble(df.format(bmr));
+		double bmr = activity(activity) * (9.99 * bodyWeight + 6.25 * bodyHeight - 4.92 * age + 5);
+		return doubleToTwoDigits(bmr);
 	}
 
 	// Katch-McArdle equation (formula)
 	public static double calcKatchMcArdle(double bodyWeight, double bodyFat, double activity) {
-		DecimalFormat df = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US));
-		// Little to no exercise Daily kilocalories needed = BMR x 1.2
-		// Light exercise (1–3 days per week) Daily kilocalories needed = BMR x
-		// 1.375
-		// Moderate exercise (3–5 days per week) Daily kilocalories needed = BMR
-		// x 1.55
-		// Heavy exercise (6–7 days per week) Daily kilocalories needed = BMR x
-		// 1.725
-		// Very heavy exercise (twice per day, extra heavy workouts) Daily
-		// kilocalories needed = BMR x 1.9
-		if (activity == 1) {
-			activity = 1.2;
-		} else if (activity == 2) {
-			activity = 1.375;
-		} else if (activity == 3) {
-			activity = 1.55;
-		} else if (activity == 4) {
-			activity = 1.725;
-		} else if (activity == 5) {
-			activity = 1.9;
-		}
-		double bmr = activity * (370 + (21.6 * bodyWeight * (1 - bodyFat)));
-		return Double.parseDouble(df.format(bmr));
+		double bmr = activity(activity) * (370 + (21.6 * bodyWeight * (1 - bodyFat)));
+		return doubleToTwoDigits(bmr);
 	}
 
 	public static double calcWorldHealthOrganization(double bodyWeight, double activity) {
-		DecimalFormat df = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US));
 		double bmr;
-		// Little to no exercise Daily kilocalories needed = BMR x 1.2
-		// Light exercise (1–3 days per week) Daily kilocalories needed = BMR x
-		// 1.375
-		// Moderate exercise (3–5 days per week) Daily kilocalories needed = BMR
-		// x 1.55
-		// Heavy exercise (6–7 days per week) Daily kilocalories needed = BMR x
-		// 1.725
-		// Very heavy exercise (twice per day, extra heavy workouts) Daily
-		// kilocalories needed = BMR x 1.9
-		if (activity == 1) {
-			activity = 1.2;
-		} else if (activity == 2) {
-			activity = 1.375;
-		} else if (activity == 3) {
-			activity = 1.55;
-		} else if (activity == 4) {
-			activity = 1.725;
-		} else if (activity == 5) {
-			activity = 1.9;
-		}
-		bmr = activity * (0.0484 * bodyWeight + 3.653) * 240;
-		return Double.parseDouble(df.format(bmr));
+		bmr = activity(activity) * (0.0484 * bodyWeight + 3.653) * 240;
+		return doubleToTwoDigits(bmr);
 	}
 
 	// Tkachenko equation (formula)
 	public static double calcTkachenko(double bodyWeight, double bodyHeightMeters, double activity) {
-		DecimalFormat df = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US));
 		double bmr;
-		// Little to no exercise Daily kilocalories needed = BMR x 1.2
-		// Light exercise (1–3 days per week) Daily kilocalories needed = BMR x
-		// 1.375
-		// Moderate exercise (3–5 days per week) Daily kilocalories needed = BMR
-		// x 1.55
-		// Heavy exercise (6–7 days per week) Daily kilocalories needed = BMR x
-		// 1.725
-		// Very heavy exercise (twice per day, extra heavy workouts) Daily
-		// kilocalories needed = BMR x 1.9
-		if (activity == 1) {
-			activity = 1.2;
-		} else if (activity == 2) {
-			activity = 1.375;
-		} else if (activity == 3) {
-			activity = 1.55;
-		} else if (activity == 4) {
-			activity = 1.725;
-		} else if (activity == 5) {
-			activity = 1.9;
-		}
-		bmr = activity * ((11.3 * bodyWeight) + (16 * bodyHeightMeters) + 901);
-		return Double.parseDouble(df.format(bmr));
+		bmr = activity(activity) * ((11.3 * bodyWeight) + (16 * bodyHeightMeters) + 901);
+		return doubleToTwoDigits(bmr);
 	}
 
 	// Body Mass Index
 	// Body mass index (BMI) is a measure of body fat based on height and weight
 	// that applies to adult men and women.
 	public static double bmi(double bodyWeight, double bodyHeightMeters) {
-		DecimalFormat df = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US));
 		// Underweight = <18.5
 		// Normal weight = 18.5–24.9
 		// Overweight = 25–29.9
@@ -179,9 +72,38 @@ public class CaloriesCalc {
 		double bmi;
 		for (int i = 90; i < 110; i++) {
 			bmi = i / (bodyHeightMeters * bodyHeightMeters);
-			System.out.println(i + "kg Body Mass Index : " + df.format(bmi));
+			System.out.println(i + "kg Body Mass Index : " + doubleToTwoDigits(bmi));
 		}
 		bmi = bodyWeight / (bodyHeightMeters * bodyHeightMeters);
-		return Double.parseDouble(df.format(bmi));
+		return doubleToTwoDigits(bmi);
+	}
+
+	private static double activity(double activity) {
+		// Little to no exercise Daily kilocalories needed = BMR x 1.2
+		// Light exercise (1–3 days per week) Daily kilocalories needed = BMR x
+		// 1.375
+		// Moderate exercise (3–5 days per week) Daily kilocalories needed = BMR
+		// x 1.55
+		// Heavy exercise (6–7 days per week) Daily kilocalories needed = BMR x
+		// 1.725
+		// Very heavy exercise (twice per day, extra heavy workouts) Daily
+		// kilocalories needed = BMR x 1.9
+		if (activity == 1) {
+			activity = 1.2;
+		} else if (activity == 2) {
+			activity = 1.375;
+		} else if (activity == 3) {
+			activity = 1.55;
+		} else if (activity == 4) {
+			activity = 1.725;
+		} else if (activity == 5) {
+			activity = 1.9;
+		}
+		return activity;
+	}
+
+	private static double doubleToTwoDigits(double number) {
+		DecimalFormat df = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US));
+		return Double.parseDouble(df.format(number));
 	}
 }
