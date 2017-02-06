6394634f4b7aaabbcac892cc0f8d0c7b543e57ca
kgit1 <kgit1@rambler.ru>
2017-02-07 00:07:36 +0200

    up

--- src/f1/Cafee/CommandPattern/RemoteControlTest.java
+++ src/f1/Cafee/CommandPattern/RemoteControlTest.java
@@ -2,18 +2,27 @@ package f1.Cafee.CommandPattern;
 
 public class RemoteControlTest {
 
-		public static void main(String[] args) {
-			
+	public static void main(String[] args) {
+
+		// crate remote control object
 		SimpleRemoteControl remote = new SimpleRemoteControl();
+		// create device object
 		Light light = new Light();
+		// create command object for command, assign device
 		LightOnCommant lightOn = new LightOnCommant(light);
+		// create device object
 		GarageDoor garageDoor = new GarageDoor();
+		// create command object for command, assign device
 		GarageDoorOpenCommand openGarage = new GarageDoorOpenCommand(garageDoor);
 
+		// assign command object to remote
 		remote.setCommand(lightOn);
+		// invoke execute method of Command object
 		remote.buttonWasPressed();
 
+		// assign command object to remote
 		remote.setCommand(openGarage);
+		// invoke execute method of Command object
 		remote.buttonWasPressed();
 
 	}
