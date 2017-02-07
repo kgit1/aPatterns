d16824e7bbea305d4cced243fec0e3dcdf69966a
kgit1 <kgit1@rambler.ru>
2017-02-07 23:25:17 +0200

    adapter pattern

--- src/h1/Duck/AdapterPattern/MainDuck.java
+++ src/h1/Duck/AdapterPattern/MainDuck.java
@@ -1,8 +1,38 @@
 package h1.Duck.AdapterPattern;
 
-public class MainDuck {
+//adapter class has inside object of adapted class and shares interface with class to which will adapt
+//and invokes methods of adapted class in overrides methods of implemented interface shared with class to which 
+//will be adapted
 
+//adapter implements interface of class to which will be adapted
+//public class TurkeyDuckAdapter implements Duck {
+//
+//	// object of class which will be adapted
+//	Turkey turkey;
+//
+//	public TurkeyDuckAdapter(Turkey turkey) {
+//		this.turkey = turkey;
+//	}
+//
+//	// overrides methods from implemented interface
+//	@Override
+//	public void quack() {
+//		// invoke method of adapted class
+//		turkey.goble();
+//	}
+//
+//	// overrides methods from implemented interface
+//	@Override
+//	public void fly() {
+//		for (int i = 0; i < 5; i++) {
+//			// invoke method of adapted class
+//			turkey.fly();
+//		}
+//	}
+//}
+public class MainDuck {
 	public static void main(String[] args) {
+
 		Duck mallardDuck = new MallardDuck();
 
 		Turkey turkey = new Turkey();
--- src/h1/Duck/AdapterPattern/TurkeyDuckAdapter.java
+++ src/h1/Duck/AdapterPattern/TurkeyDuckAdapter.java
@@ -1,23 +1,28 @@
 package h1.Duck.AdapterPattern;
 
+//adapter implements interface of class to which will be adapted
 public class TurkeyDuckAdapter implements Duck {
 
+	// object of class which will be adapted
 	Turkey turkey;
 
 	public TurkeyDuckAdapter(Turkey turkey) {
 		this.turkey = turkey;
 	}
 
+	// overrides methods from implemented interface
 	@Override
 	public void quack() {
+		// invoke method of adapted class
 		turkey.goble();
 	}
 
+	// overrides methods from implemented interface
 	@Override
 	public void fly() {
 		for (int i = 0; i < 5; i++) {
+			// invoke method of adapted class
 			turkey.fly();
 		}
 	}
-
 }
