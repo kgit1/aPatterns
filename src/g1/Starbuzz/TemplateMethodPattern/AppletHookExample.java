a272ab4fd4ba369331967acfd1e0bf264316800f
kgit1 <kgit1@rambler.ru>
2017-02-09 01:52:40 +0200

    up

--- src/g1/Starbuzz/TemplateMethodPattern/AppletHookExample.java
+++ src/g1/Starbuzz/TemplateMethodPattern/AppletHookExample.java
@@ -7,27 +7,31 @@ public class AppletHookExample extends Applet {
 
 	String message;
 
+	// hook here - init() method, hook method - method which has empty or some
+	// default realization in parent class, and which we can override to
+	// implement our behavior in this part of algorithm given by parent class
 	public void init() {
 		message = "Hello World, I'm alive";
 		repaint();
 	}
 
+	// hook here - start()
 	public void start() {
 		message = "Now i'm starting up";
 		repaint();
 	}
 
+	// hook here - stop() method
 	public void stop() {
 		message = "Oh, now i'm being stoped";
 		repaint();
 	}
 
+	// hook here - destroy() method
 	public void destroy() {
 	}
 
-	// hook here - paint() method, hook method - method which has empty or some
-	// default realization in parent class, and which we can override to
-	// implement our behavior in this part of algorithm given by parent class
+	// hook here - paint() method
 	public void paint(Graphics graphics) {
 		graphics.drawString(message, 100, 100);
 	}
