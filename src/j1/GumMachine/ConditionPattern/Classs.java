51ac0e3e9d8ca331a8b9bd01d2bcdbbf4698a38c
kgit1 <kgit1@rambler.ru>
2017-02-11 23:17:49 +0200

    up

--- src/j1/GumMachine/ConditionPattern/Classs.java
+++ src/j1/GumMachine/ConditionPattern/Classs.java
@@ -3,9 +3,22 @@ package j1.GumMachine.ConditionPattern;
 public class Classs {
 
 	final static int SOLD_OUT = 0;
-	final static int No_QUARTER = 1;
+	final static int NO_QUARTER = 1;
 	final static int HAS_QUARTER = 2;
 	final static int SOLD = 3;
 	int state = SOLD_OUT;
+
+	public void insertQuarter() {
+		if (state == HAS_QUARTER) {
+			System.out.println("You can't insert another quarter");
+		} else if (state == SOLD_OUT) {
+			System.out.println("You can't insert a quarter, the machine is sold out");
+		} else if (state == SOLD) {
+			System.out.println("Please wait, we're already giving you a gumball");
+		} else if (state == NO_QUARTER) {
+			state = HAS_QUARTER;
+			System.out.println("You inserted a quarter");
+		}
+	}
 
 }
