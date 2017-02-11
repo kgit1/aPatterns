fec871b95af3d557fba066a4dae19f0732713f5a
kgit1 <kgit1@rambler.ru>
2017-02-11 23:14:20 +0200

    Condition pattern

---
+++ src/j1/GumMachine/ConditionPattern/Classs.java
@@ -0,0 +1,11 @@
+package j1.GumMachine.ConditionPattern;
+
+public class Classs {
+
+	final static int SOLD_OUT = 0;
+	final static int No_QUARTER = 1;
+	final static int HAS_QUARTER = 2;
+	final static int SOLD = 3;
+	int state = SOLD_OUT;
+
+}
