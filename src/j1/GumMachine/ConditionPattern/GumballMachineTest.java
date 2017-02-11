6cb739a9edcfd33f8da4bfc6a7d2ee17e04a2900
kgit1 <kgit1@rambler.ru>
2017-02-12 00:29:48 +0200

    up

---
+++ src/j1/GumMachine/ConditionPattern/GumballMachineTest.java
@@ -0,0 +1,36 @@
+package j1.GumMachine.ConditionPattern;
+
+public class GumballMachineTest {
+
+	public static void main(String[] args) {
+
+		GumballMachine gumballMachine = new GumballMachine(5);
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.ejectQuarter();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+	}
+
+}
