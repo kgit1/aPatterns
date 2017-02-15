c5bace414dbc7d7e68d2c50ba9ca35a44892e905
kgit1 <kgit1@rambler.ru>
2017-02-16 00:55:42 +0200

    up

--- src/k3/VirtualProxy/ProxyPattern/ImageProxyTest.java
+++ src/k3/VirtualProxy/ProxyPattern/ImageProxyTest.java
@@ -5,7 +5,9 @@ import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map.Entry;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -14,10 +16,15 @@ import javax.swing.JMenuItem;
 
 public class ImageProxyTest {
 	ImageComponent imageComponent;
+
+	// create JFrame
 	JFrame frame = new JFrame("CD Cover Viewer");
+	// menu bar
 	JMenuBar menuBar;
+	// menu
 	JMenu menu;
-	Hashtable cds = new Hashtable();
+	// create map to hold sets of "Album name" - "Cover image url"
+	HashMap<String, String> cds = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
 		ImageProxyTest testDrive = new ImageProxyTest();
@@ -32,37 +39,92 @@ public class ImageProxyTest {
 		cds.put("Northern Exposure", "http://images.amazon.com/images/P/B000003SFN.01.LZZZZZZZ.jpg");
 		cds.put("Selected Ambient Works, Vol. 2", "http://images.amazon.com/images/P/B000002MNZ.01.LZZZZZZZ.jpg");
 
+		// create new URL and pass value from pa - got buy given key
 		URL initialURL = new URL((String) cds.get("Selected Ambient Works, Vol. 2"));
+		// create menu bar object
 		menuBar = new JMenuBar();
+		// create menu object with given title
 		menu = new JMenu("Favorite CDs");
+		// add menu object to menu bar object
 		menuBar.add(menu);
+		// put menu bar object on frame
 		frame.setJMenuBar(menuBar);
 
-		for (Enumeration e = cds.keys(); e.hasMoreElements();) {
-			String name = (String) e.nextElement();
+		// fill menu object with items
+		// by iteration through maps keySet, got by keySet() method
+		for (String string : cds.keySet()) {
+			String name = string;
+			// create nem menu element with given title
 			JMenuItem menuItem = new JMenuItem(name);
+			// add menu element to menu
 			menu.add(menuItem);
-			menuItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					imageComponent.setIcon(new ImageProxy(getCDUrl(event.getActionCommand())));
-					frame.repaint();
-				}
-			});
+			// add action listener to menu item
+			menuItem.addActionListener(
+					// action listener realized with anonymous class
+					// ActionListener
+					new ActionListener() {
+						// method actionPerformed of ActionListenerClass
+						public void actionPerformed(ActionEvent event) {
+							// call set icon method from imageComponent
+							imageComponent.setIcon(
+									// give new ImageProxy object
+									new ImageProxy(
+											// give URL by invoking getCDUrl()
+											// method which buy name pulls
+											// string with url from map of urls,
+											// using name as key
+											// and transform received string to
+											// url
+											getCDUrl(
+													// give name to getCDURL
+													// method
+													// obtaining it by invoking
+													// getActionCommand() which
+													// returns the command
+													// string associated with
+													// this action
+													event.getActionCommand())));
+							frame.repaint();
+						}
+					});
 		}
+
+		// for (Enumeration e = cds.keys(); e.hasMoreElements();) {
+		// String name = (String) e.nextElement();
+		// JMenuItem menuItem = new JMenuItem(name);
+		// menu.add(menuItem);
+		// menuItem.addActionListener(new ActionListener() {
+		// public void actionPerformed(ActionEvent event) {
+		// imageComponent.setIcon(new
+		// ImageProxy(getCDUrl(event.getActionCommand())));
+		// frame.repaint();
+		// }
+		// });
+		// }
 
 		// set up frame and menus
 
+		// create new icon object by initializing url
 		Icon icon = new ImageProxy(initialURL);
+		// create new imageComponent and give it an icon object
 		imageComponent = new ImageComponent(icon);
+		// add imageComponent on the frame
 		frame.getContentPane().add(imageComponent);
+		// set close operation
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// give sizes to frame
 		frame.setSize(800, 600);
+		// make frame(window) visible
 		frame.setVisible(true);
 
 	}
 
+	// method which buy name pulls
+	// string with url from map of urls,
+	// using name as key
 	URL getCDUrl(String name) {
 		try {
+			// and transform received string to url
 			return new URL((String) cds.get(name));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
