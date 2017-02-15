31604dad5e82f7c412f841344bf862cbec65e37e
kgit1 <kgit1@rambler.ru>
2017-02-15 23:33:28 +0200

    up

--- src/k3/VirtualProxy/ProxyPattern/ImageProxy.java
+++ src/k3/VirtualProxy/ProxyPattern/ImageProxy.java
@@ -34,24 +34,37 @@ class ImageProxy implements Icon {
 		}
 	}
 
+	// method to paint image on screen
 	@Override
 	public void paintIcon(final Component c, Graphics g, int x, int y) {
 		if (imageIcon != null) {
+			// if object already exists - invoke method from it
 			imageIcon.paintIcon(c, g, x, y);
+			// if not - show loading message
 		} else {
 			g.drawString("Loading CD covew, please wait...,", x + 300, y + 190);
+			// if image not loading
 			if (!retrieving) {
+				// turn loading flag to true and load image from url
 				retrieving = true;
-				retrievalThread = new Thread(new Runnable() {
-					public void run() {
-						try {
-							imageIcon = new ImageIcon(imageUrl, "CD cover");
-							c.repaint();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				});
+				// give anonymous thread realization to our retrieving thread
+				retrievalThread =
+						// anonymous thread class
+						new Thread(new Runnable() {
+							// method run of anonyous thread class
+							public void run() {
+								try {
+									// load image from url
+									imageIcon = new ImageIcon(imageUrl, "CD cover");
+									// call repaint method after getting actual
+									// image from url
+									c.repaint();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+						});
+				// start retrieving thread
 				retrievalThread.start();
 			}
 		}
