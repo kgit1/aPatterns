38734b6bb4552c1e9ef20afb4795484473c1f950
kgit1 <kgit1@rambler.ru>
2017-02-16 23:32:12 +0200

    up

--- src/k4/Dating/SecurityProxy/ProxyPattern/MatchMakingTest.java
+++ src/k4/Dating/SecurityProxy/ProxyPattern/MatchMakingTest.java
@@ -6,6 +6,41 @@ public class MatchMakingTest {
 
 	public static void main(String[] args) {
 
+		MatchMakingTest test = new MatchMakingTest();
+
+	}
+
+	public MatchMakingTest() {
+		initializeDatabase();
+	}
+
+	public void drive() {
+		PersonBean joe = getPersonFromDatabase("Joe Javabean");
+		PersonBean ownerProxy = getOwnerProxy(joe);
+		System.out.println("Name is " + ownerProxy.getName());
+		ownerProxy.setInterests("bowling, Go");
+		System.out.println("Interests set from owner proxy");
+
+		try {
+			ownerProxy.setHotOrNotRating(10);
+		} catch (Exception e) {
+			System.out.println("Can't set rating from own proxy");
+			e.printStackTrace();
+		}
+		System.out.println("Rating is : " + ownerProxy.getHotOrNotRating());
+
+		PersonBean nonOwnerProxy = getNonOwnerProxy(joe);
+
+		System.out.println("Name is : " + nonOwnerProxy.getName());
+		try {
+			nonOwnerProxy.setInterests("bowling,go");
+		} catch (Exception e) {
+			System.out.println("Can't set interests from nonOwnProxy");
+			e.printStackTrace();
+		}
+		nonOwnerProxy.setHotOrNotRating(3);
+		System.out.println("Rating set from nonOwnProxy");
+		System.out.println("Rating is : " + nonOwnerProxy.getHotOrNotRating());
 	}
 
 	// Proxy provides static methods for creating dynamic proxy
