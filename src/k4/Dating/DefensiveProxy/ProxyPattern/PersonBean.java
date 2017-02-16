d3549276dfbc5e7d75c79c8188e8d57ab5945897
kgit1 <kgit1@rambler.ru>
2017-02-16 22:59:29 +0200

    SecurityProxe proxy pattern

---
+++ src/k4/Dating/DefensiveProxy/ProxyPattern/PersonBean.java
@@ -0,0 +1,19 @@
+package k4.Dating.DefensiveProxy.ProxyPattern;
+
+public interface PersonBean {
+	String getName();
+
+	String getGender();
+
+	String getInterests();
+
+	int getHotOrNotRating();
+
+	void setName(String name);
+
+	void setGender(String gender);
+
+	void setInterests(String interests);
+
+	void setHotOrNotRating(int rating);
+}
