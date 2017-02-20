eaf7a14bc1f21679aa2e22057c9f89305e117a85
kgit1 <kgit1@rambler.ru>
2017-02-20 20:56:43 +0200

    duck pattern coposite

---
+++ src/l1/Duck/PatternsOfPattern/DuckSimulatorMain2Factory2Composite.java
@@ -0,0 +1,43 @@
+package l1.Duck.PatternsOfPattern;
+
+public class DuckSimulatorMain2Factory2Composite {
+
+	public static void main(String[] args) {
+		DuckSimulatorMain2Factory2Composite simulator = new DuckSimulatorMain2Factory2Composite();
+		AbstractDuckFactory factory = new CountingDuckFactory();
+		simulator.simulate(factory);
+	}
+
+	void simulate(AbstractDuckFactory factory) {
+		Quackable mallardDuck = factory.createMallardDuck();
+		Quackable redheadDuck = factory.createRedheadDuck();
+		Quackable duckCall = factory.createDuckCall();
+		Quackable rubberDcuk = factory.createRubberDuck();
+		Quackable goose = new QuackCounter(new GooseAdapter(new Goose()));
+
+		Flock flockOfDucks = new Flock();
+		flockOfDucks.add(mallardDuck);
+		flockOfDucks.add(redheadDuck);
+		flockOfDucks.add(duckCall);
+		flockOfDucks.add(rubberDcuk);
+		flockOfDucks.add(goose);
+
+		Flock flockOfMallards = new Flock();
+		flockOfMallards.add(factory.createMallardDuck());
+		flockOfMallards.add(factory.createMallardDuck());
+		flockOfMallards.add(factory.createMallardDuck());
+		flockOfMallards.add(factory.createMallardDuck());
+		flockOfMallards.add(factory.createMallardDuck());
+		flockOfMallards.add(factory.createMallardDuck());
+
+		System.out.println("\nDuck Simulator");
+
+		simulate(flockOfDucks);
+		simulate(flockOfMallards);
+		System.out.println(QuackCounter.getQuacks());
+	}
+
+	void simulate(Quackable quackable) {
+		quackable.quack();
+	}
+}
---
+++ src/l1/Duck/PatternsOfPattern/Flock.java
@@ -0,0 +1,26 @@
+package l1.Duck.PatternsOfPattern;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+// composite pattern is a partitioning design pattern.The composite pattern describes that a group 
+//of objects is to be treated in the same way as a single instance of an object.The intent of a composite 
+//is to"compose"objects into tree structures to represent part-whole hierarchies.Implementing the composite 
+//pattern lets clients treat individual objects and compositions uniformly
+public class Flock implements Quackable {
+
+	ArrayList<Quackable> quackers = new ArrayList<>();
+
+	public void add(Quackable quacker) {
+		quackers.add(quacker);
+	}
+
+	@Override
+	public void quack() {
+		Iterator iterator = quackers.iterator();
+		while (iterator.hasNext()) {
+			Quackable quacker = (Quackable) iterator.next();
+			quacker.quack();
+		}
+	}
+}
