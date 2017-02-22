33ade9d9fdda79dc6bf03eb8a5d065c28696108b
kgit1 <kgit1@rambler.ru>
2017-02-22 22:51:02 +0200

    up

--- src/l2/DJMix/PatternOfPatterns/MVC/ModelViewController/DJView.java
+++ src/l2/DJMix/PatternOfPatterns/MVC/ModelViewController/DJView.java
@@ -1,5 +1,8 @@
 package l2.DJMix.PatternOfPatterns.MVC.ModelViewController;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,6 +14,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class DJView implements ActionListener, BeatObserver, BPMObserver {
 
@@ -22,10 +26,10 @@ public class DJView implements ActionListener, BeatObserver, BPMObserver {
 	JPanel viewPanel;
 	BeatBar beatBar;
 	JLabel bpmOutputLabel;
-	JFrame controllFrame;
-	JPanel controllPanel;
-	JLabel bmpLabel;
-	JTextField bmpTextField;
+	JFrame controlFrame;
+	JPanel controlPanel;
+	JLabel bpmLabel;
+	JTextField bpmTextField;
 	JButton setBPMButton;
 	JButton increaseBPMButton;
 	JButton decreaseBPMButton;
@@ -42,6 +46,89 @@ public class DJView implements ActionListener, BeatObserver, BPMObserver {
 	}
 
 	public void createView() {
+		viewPanel = new JPanel(new GridLayout(1, 2));
+		viewFrame = new JFrame("View");
+		viewFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		viewFrame.setSize(new Dimension(100, 80));
+		// SwingConstants - a collection of constants generally used for
+		// positioning and orienting components on the screen.
+		// LEFT - box-orientation constant used to specify the left side of a
+		// box.
+		bpmOutputLabel = new JLabel("offline", SwingConstants.CENTER);
+		beatBar = new BeatBar();
+		beatBar.setValue(0);
+		JPanel bpmPanel = new JPanel(new GridLayout(2, 1));
+		bpmPanel.add(beatBar);
+		bpmPanel.add(bpmOutputLabel);
+		viewPanel.add(bpmPanel);
+		viewFrame.getContentPane().add(viewPanel, BorderLayout.CENTER);
+		viewFrame.pack();
+		viewFrame.setVisible(true);
+	}
+
+	public void createControls() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		controlFrame = new JFrame("Control");
+		controlFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		controlFrame.setSize(new Dimension(100, 80));
+
+		controlPanel = new JPanel(new GridLayout(1, 2));
+
+		menuBar = new JMenuBar();
+		menu = new JMenu("DJ Control");
+		startMenuItem = new JMenuItem("Start");
+		menu.add(startMenuItem);
+		startMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controller.start();
+			}
+		});
+
+		stopMenuItem = new JMenuItem("Stop");
+		menu.add(stopMenuItem);
+		stopMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controller.stop();
+				// bpmOutputLabel.setText("offline");
+			}
+		});
+
+		JMenuItem exit = new JMenuItem("Quit");
+		menu.add(exit);
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Terminates the currently running Java Virtual Machine. The
+				// argument serves as a status code; by convention, a nonzero
+				// status code indicates abnormal termination.
+				System.exit(0);
+			}
+		});
+
+		menuBar.add(menu);
+		controlFrame.setJMenuBar(menuBar);
+
+		bpmTextField = new JTextField(2);
+		// SwingConstants - a collection of constants generally used for
+		// positioning and orienting components on the screen.
+		// RIGHT - box-orientation constant used to specify the right side of a
+		// box.
+		bpmLabel = new JLabel("Enter BPM: ", SwingConstants.RIGHT);
+		setBPMButton = new JButton("Set");
+		setBPMButton.setSize(new Dimension(10, 40));
+		increaseBPMButton = new JButton(">>");
+		decreaseBPMButton = new JButton("<<");
+		setBPMButton.addActionListener(this);
+		increaseBPMButton.addActionListener(this);
+		decreaseBPMButton.addActionListener(this);
+
+		JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+
+		buttonPanel.add(decreaseBPMButton);
+		buttonPanel.add(increaseBPMButton);
+
 	}
 
 	public void updateBPM() {
