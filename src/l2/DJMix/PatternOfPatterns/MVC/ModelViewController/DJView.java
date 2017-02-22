c3e0a5fbeb7b3a1e106b65ace7a8bedcc2399161
kgit1 <kgit1@rambler.ru>
2017-02-22 21:34:39 +0200

    up

--- src/l2/DJMix/PatternOfPatterns/MVC/ModelViewController/DJView.java
+++ src/l2/DJMix/PatternOfPatterns/MVC/ModelViewController/DJView.java
@@ -3,18 +3,36 @@ package l2.DJMix.PatternOfPatterns.MVC.ModelViewController;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class DJView implements ActionListener, BeatObserver, BPMObserver {
 
+	// refer to model object
 	BeatModelInterface model;
+	// refer to controller object
 	ControllerInterface controller;
-	JFrame jFrame;
-	JPanel jpanel;
+	JFrame viewFrame;
+	JPanel viewPanel;
 	BeatBar beatBar;
 	JLabel bpmOutputLabel;
+	JFrame controllFrame;
+	JPanel controllPanel;
+	JLabel bmpLabel;
+	JTextField bmpTextField;
+	JButton setBPMButton;
+	JButton increaseBPMButton;
+	JButton decreaseBPMButton;
+	JMenuBar menuBar;
+	JMenu menu;
+	JMenuItem startMenuItem;
+	JMenuItem stopMenuItem;
 
 	public DJView(ControllerInterface controller, BeatModelInterface model) {
 		this.controller = controller;
