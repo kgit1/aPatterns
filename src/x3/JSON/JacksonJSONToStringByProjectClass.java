fbb6bb34c9cda65d9afaa178d0469e46a14380b0
kgit1 <kgit1@rambler.ru>
2017-04-10 20:30:40 +0300

    up

--- src/x3/JSON/JacksonJSONToStringByProjectClass.java
+++ src/x3/JSON/JacksonJSONToStringByProjectClass.java
@@ -2,7 +2,6 @@ package x3.JSON;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
--- src/x3/JSON/Persons.java
+++ src/x3/JSON/Persons.java
@@ -1,6 +1,5 @@
 package x3.JSON;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Persons extends HashMap<String, HashMap<String,Person>>{
