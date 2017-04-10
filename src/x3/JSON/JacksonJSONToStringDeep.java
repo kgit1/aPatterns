38bf05f2f44413af5e3c2ee590cb1356a379207e
kgit1 <kgit1@rambler.ru>
2017-04-10 20:06:45 +0300

    up

--- src/x3/JSON/JacksonJSONToStringDeep.java
+++ src/x3/JSON/JacksonJSONToStringDeep.java
@@ -55,41 +55,41 @@ public class JacksonJSONToStringDeep {
 // Summarize other answers and what I known, I found 10 main ways to do this
 // (see below). And I wrote some performance tests (see results below), for
 // example, if we want to find sum of all keys and values of map, we can write :
-//
+
 // Using iterator and Map.Entry
 // long i = 0;
 // Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
 // while (it.hasNext()) {
 // Map.Entry<Integer, Integer> pair = it.next();
-// i += pair.getKey() + pair.getValue();
-// }
+// i += pair.getKey() + pair.getValue();}
+
 // Using foreach and Map.Entry
 // long i = 0;
 // for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
-// i += pair.getKey() + pair.getValue();
-// }
+// i += pair.getKey() + pair.getValue();}
+
 // Using forEach from Java 8
 // final long[] i = {0};
 // map.forEach((k, v) -> i[0] += k + v);
 // Using keySet and foreach
 // long i = 0;
 // for (Integer key : map.keySet()) {
-// i += key + map.get(key);
-// }
+// i += key + map.get(key);}
+
 // Using keySet and iterator
 // long i = 0;
 // Iterator<Integer> itr2 = map.keySet().iterator();
 // while (itr2.hasNext()) {
 // Integer key = itr2.next();
-// i += key + map.get(key);
-// }
+// i += key + map.get(key);}
+
 // Using for and Map.Entry
 // long i = 0;
 // for (Iterator<Map.Entry<Integer, Integer>> entries =
 // map.entrySet().iterator(); entries.hasNext(); ) {
 // Map.Entry<Integer, Integer> entry = entries.next();
-// i += entry.getKey() + entry.getValue();
-// }
+// i += entry.getKey() + entry.getValue();}
+
 // Using Java 8 Stream Api
 // final long[] i = {0};
 // map.entrySet().stream().forEach(e -> i[0] += e.getKey() + e.getValue());
@@ -101,18 +101,18 @@ public class JacksonJSONToStringDeep {
 // long i = 0;
 // MapIterator<Integer, Integer> it = iterableMap.mapIterator();
 // while (it.hasNext()) {
-// i += it.next() + it.getValue();
-// }
+// i += it.next() + it.getValue();}
+
 // Using MutableMap of Eclipse (CS) collections
 // final long[] i = {0};
 // mutableMap.forEachKeyValue((key, value) -> {
-// i[0] += key + value;
-// });
+// i[0] += key + value;});
+
 // Perfomance tests (mode = AverageTime, system = Win 8.1 64-bit, Intel i7-4790
 // 3.60GHz 3.60GHz, 16 GB)
-//
+
 // 1) For small map (100 elements), score 0.308 is the best
-//
+
 // Benchmark Mode Cnt Score Error Units
 // test3_UsingForEachAndJava8 avgt 10 0.308 ± 0.021 us/op
 // test10_UsingEclipseMap avgt 10 0.309 ± 0.009 us/op
@@ -125,7 +125,7 @@ public class JacksonJSONToStringDeep {
 // test5_UsingKeySetAndIterator avgt 10 0.863 ± 0.025 us/op
 // test8_UsingJava8StreamApiParallel avgt 10 5.552 ± 0.185 us/op
 // 2) For map with 10000 elements, score 37.606 is the best
-//
+
 // Benchmark Mode Cnt Score Error Units
 // test10_UsingEclipseMap avgt 10 37.606 ± 0.790 us/op
 // test3_UsingForEachAndJava8 avgt 10 50.368 ± 0.887 us/op
@@ -138,7 +138,7 @@ public class JacksonJSONToStringDeep {
 // test8_UsingJava8StreamApiParallel avgt 10 112.511 ± 0.365 us/op
 // test9_UsingApacheIterableMap avgt 10 125.714 ± 1.935 us/op
 // 3) For map with 100000 elements, score 1184.767 is the best
-//
+
 // Benchmark Mode Cnt Score Error Units
 // test1_UsingWhileAndMapEntry avgt 10 1184.767 ± 332.968 us/op
 // test10_UsingEclipseMap avgt 10 1191.735 ± 304.273 us/op
@@ -151,11 +151,11 @@ public class JacksonJSONToStringDeep {
 // test7_UsingJava8StreamApi avgt 10 1706.676 ± 436.867 us/op
 // test9_UsingApacheIterableMap avgt 10 3289.866 ± 1445.564 us/op
 // Graphs (perfomance tests depending on map size)
-//
+
 // enter image description here
-//
+
 // Table (perfomance tests depending on map size)
-//
+
 // 100 600 1100 1600 2100
 // test10 0.333 1.631 2.752 5.937 8.024
 // test3 0.309 1.971 4.147 8.147 10.473
