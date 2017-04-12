ddb0e32adc6ec3f626c13e80c2ad4bde3d3244d6
kgit1 <kgit1@rambler.ru>
2017-04-12 03:29:45 +0300

    FinalGGPostSendCOnnectionTest

---
+++ src/xx/WebSocket/FinalGGPostSendCOnnectionTest.java
@@ -0,0 +1,39 @@
+package xx.WebSocket;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FinalGGPostSendCOnnectionTest {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://goodgame.ru/ajax/chatlogin");
+        Map<String,Object> params = new LinkedHashMap<>();
+        params.put("login", "kirtt");
+        params.put("password", "qwert111");
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+        for (int c; (c = in.read()) >= 0;)
+            System.out.print((char)c);
+    }
+}
